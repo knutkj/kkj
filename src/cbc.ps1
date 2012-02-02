2bc56480222099a81df54c27a5c893330e6a493e
Knut Kristian Johansen <knutkj@gmail.com>
2012-02-02 22:59:58 +0100

    Updated CbC NuGet package project URL. Specified a parameter in CbC build script.

--- src/cbc.ps1
+++ src/cbc.ps1
@@ -2,7 +2,7 @@ Remove-Item -Path .\cbc*.nupkg
 Copy-Item -Path .\cbc.assert.js -Destination .\cbc.js
 $sourceCode = Get-Content -Path .\cbc.assert.js | Out-String
 Add-Type -Path .\packages\AjaxMin.4.44.4396.18853\lib\net20\AjaxMin.dll
-$minifier = New-Object Microsoft.Ajax.Utilities.Minifier
+$minifier = New-Object -TypeName Microsoft.Ajax.Utilities.Minifier
 $minifier.MinifyJavaScript($sourceCode) |
     Out-File -FilePath cbc.min.js
 Copy-Item -Path .\cbc.min.js -Destination .\cbc.assert.min.js
